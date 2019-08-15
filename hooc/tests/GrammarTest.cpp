66eaed6c6a8867d9cf6a512aa6ed004e672bd344
Benoy Bose <benoybose@gmail.com>
2019-08-15 14:50:21 +0530

    Removed unused test.

--- hooc/tests/GrammarTest.cpp
+++ hooc/tests/GrammarTest.cpp
@@ -19,47 +19,6 @@
 #define BOOST_TEST_MODULE "Grammar Test"
 #include <boost/test/included/unit_test.hpp>
 
-#include "compiler/ParserDriver.hh"
-#include "antlr4-runtime.h"
-#include "ast/Unit.h"
-#include "ast/Statement.hh"
-#include "ast/Declaration.hh"
-#include "ast/TypeSpecification.hh"
-
-
-#include <boost/test/unit_test.hpp>
-#include <boost/filesystem.hpp>
-#include <string>
-#include <ast/LiteralExpression.hh>
-
-using namespace std;
-using namespace hooc;
-using namespace hooc::compiler;
-using namespace hooc::ast;
-
 BOOST_AUTO_TEST_SUITE(GrammarTest)
-
-    BOOST_AUTO_TEST_CASE(ClassDefinision) {
-//        auto source_path = boost::filesystem::absolute("test.hoo").string();
-//        const std::string source = "namespace test.grammar;\n"
-//                                   "\n"
-//                                   "class Application\n"
-//                                   "{\n"
-//                                   "\tfunc:int (args:string[]) main\n"
-//                                   "\t{\n"
-//                                   "\t\tvar result = add(10, 45);\n"
-//                                   "\t\tconsole.print(result);\n"
-//                                   "\t}\n"
-//                                   "\t\n"
-//                                   "\tfunc:int(a:int, b:int) add\n"
-//                                   "\t{\n"
-//                                   "\t\treturn a + b;\n"
-//                                   "\t}\n"
-//                                   "}";
-//        ParserDriver driver(source, source_path);
-//        auto unit = driver.BuildModule();
-    }
-
-
 
 BOOST_AUTO_TEST_SUITE_END()
