20fe29ab049633cacd70b8458c51929ba26a36c6
Benoy Bose <benoybose@gmail.com>
2019-10-18 13:27:54 +0530

    Tested function declaration of a definition.

--- hooc/tests/Function001.cpp
+++ hooc/tests/Function001.cpp
@@ -28,6 +28,7 @@
 #include <boost/filesystem.hpp>
 #include <ast/ExpressionStatement.hh>
 #include <ast/FunctionDefinition.hh>
+#include <ast/BasicDataTypeSpecification.hh>
 
 using namespace std;
 using namespace hooc;
@@ -45,7 +46,27 @@ BOOST_AUTO_TEST_SUITE(Function001)
         auto items = unit->GetItems();
         auto first_item = *(items.begin());
         BOOST_CHECK_EQUAL(UNIT_ITEM_DEFINITION, first_item->GetUnitItemType());
-        auto definition = (FunctionDefinition* ) first_item;
+        auto definition = (Definition* ) first_item;
+        BOOST_CHECK_EQUAL(DEFINITION_FUNCTION, definition->GetDefinitionType());
+        auto func_def = (FunctionDefinition*) definition;
+        auto func_decl = func_def->GetDeclaration();
+        BOOST_CHECK_EQUAL("add", func_decl->GetName());
+        auto func_return_type = func_decl->GetReturnType();
+        BOOST_CHECK_NE(nullptr, func_return_type);
+        BOOST_CHECK_EQUAL(TYPE_SPEC_BASIC, func_return_type->GetType());
+        BOOST_CHECK_EQUAL("int", func_return_type->GetName());
+        auto basic_type = (BasicDataTypeSpecification *) func_return_type;
+        BOOST_CHECK_EQUAL(BASIC_DATA_TYPE_INT, basic_type->GetDataType());
+        auto parameters = func_decl->GetParamList();
+        BOOST_CHECK_EQUAL(2, parameters.size());
+        auto param1 = *(parameters.begin());
+        BOOST_CHECK_EQUAL("a", param1->GetName());
+        BOOST_CHECK_EQUAL("int", param1->GetDelcaredType()->GetName());
+        auto param2 = *(++ parameters.begin());
+        BOOST_CHECK_EQUAL("b", param2->GetName());
+        BOOST_CHECK_EQUAL("int", param2->GetDelcaredType()->GetName());
+        auto statements = func_def->GetStatements();
+        BOOST_CHECK_EQUAL(STMT_COMPOUND, statements->GetStatementType());
     }
 
 BOOST_AUTO_TEST_SUITE_END()
