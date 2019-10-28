c4bbebca09b086916378c64b96092f4bcb4f557b
Benoy Bose <benoybose@gmail.com>
2019-10-28 20:36:30 +0530

    Tested code generation for 'func foo(a:int, b:int, c:int, d:int) {}'

--- hooc/tests/WindowsX64EmitterTest.cpp
+++ hooc/tests/WindowsX64EmitterTest.cpp
@@ -130,4 +130,32 @@ BOOST_AUTO_TEST_SUITE(WindowsX644EmitterTest)
         BOOST_CHECK(VerifyByteVector(buffer, expected, 19));
     }
 
+    BOOST_AUTO_TEST_CASE(TEST05) {
+        const std::string source = "func foo(a:int, b:int, c:int, d:int) {}";
+        ParserDriver driver(source, "foo.hoo");
+        auto module = driver.BuildModule();
+        BOOST_ASSERT(module->Success());
+        auto unit = module->GetUnit();
+        auto func = (FunctionDefinition *) (*unit->GetItems().begin());
+        BOOST_ASSERT("foo" == func->GetDeclaration()->GetName());
+        WindowsX64Emitter emitter(unit);
+        auto code = emitter.GenerateCode(func);
+        BOOST_CHECK_EQUAL(CODE_TYPE_FUNCTION, code->GetType());
+        BOOST_CHECK_EQUAL("_Z3fooxxxx", code->GetName());
+        BOOST_CHECK_EQUAL(23, code->GetSize());
+        const auto &buffer = code->GetBuffer();
+        BOOST_CHECK_EQUAL(23, buffer.size());
+        byte expected[23] = {
+                0x55,
+                0x48, 0x89, 0xe5,
+                0x48, 0x89, 0x4d, 0x10,
+                0x48, 0x89, 0x55, 0x18,
+                0x4c, 0x89, 0x45, 0x20,
+                0x4c, 0x89, 0x4d, 0x28,
+                0x90,
+                0x5d,
+                0xc3};
+        BOOST_CHECK(VerifyByteVector(buffer, expected, 23));
+    }
+
 BOOST_AUTO_TEST_SUITE_END()
